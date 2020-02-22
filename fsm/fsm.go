62a42e97ddd197ccc42ee3f75cfa261b97aa18b2
Nadia <nadiagw@stud.ntnu.no>
2020-02-22 13:20:49 +0100

    Add curr_dir variable
    Clean up, added pseudo code

--- fsm/fsm.go
+++ fsm/fsm.go
@@ -9,6 +9,7 @@ import (
 
 const numFloors = 4
 var orders[numFloors*3] bool // [. . .   . . .   . . .   . . . ] (3 x 1.etj, 3 x 2.etj ....)
+var curr_dir io.MotorDirection
 
 func RemoveOrdersInFloor(floor int) {
     for i:=0; i < 3; i++ { // up, down, cab
@@ -19,9 +20,11 @@ func RemoveOrdersInFloor(floor int) {
 
 func Fsm(drv_buttons chan io.ButtonEvent, drv_floors chan int){
     Door_timer := time.NewTimer(120*time.Second) //init door timer
+    //var dir_i io.ButtonType
 
     //INIT PHASE
     var d io.MotorDirection = io.MD_Up
+    curr_dir = d
     io.SetMotorDirection(d)
     curr_floor :=<-drv_floors //wait until reaches floor
     d=io.MD_Stop
@@ -54,30 +57,52 @@ func Fsm(drv_buttons chan io.ButtonEvent, drv_floors chan int){
                 if (d==io.MD_Stop){ // Idle state
                     if (a.Floor==curr_floor) {
                         Door_timer = time.NewTimer(3*time.Second)
-                        RemoveOrdersInFloor(a.Floor)
+                        RemoveOrdersInFloor(curr_floor)
                     } else if (a.Floor>curr_floor) {
                         d=io.MD_Up
+                        curr_dir = d
                         io.SetMotorDirection(d)
                     } else if (a.Floor<curr_floor) {
                         d=io.MD_Down
+                        curr_dir = d
                         io.SetMotorDirection(d)
                     }
                 }
                 
             case a := <- drv_floors:
-                curr_floor=a
+                curr_floor = a
+
+                /*
+                //Take only orders in same direction osv
+                //Remap
+                if (curr_dir == io.MD_Up) {
+                    dir_i = io.BT_HallUp
+                } else {
+                    dir_i = io.BT_HallDown
+                }
+
+                if (orders[curr_floor*3 + int(dir_i)] || orders[curr_floor*3 + int(io.BT_Cab)]) { // if (order in same dir OR cab order)
+                    d=io.MD_Stop
+                    io.SetMotorDirection(d)
+                    RemoveOrdersInFloor(curr_floor)
+                    
+                    io.SetDoorOpenLamp(true)
+                    Door_timer = time.NewTimer(3*time.Second)
+                }
+                */
+
+                // Take all orders
                 for i:=0; i < 3; i++ { // i : up, down, cab
                     if (orders[a*3 + i]) { // if order in floor 
-                        // TODO, IF ALSO IN RIGHT DIRECTION
                         d=io.MD_Stop
                         io.SetMotorDirection(d)
                         RemoveOrdersInFloor(a)
                         
-                        //Open door evt, kjøre til neste order
                         io.SetDoorOpenLamp(true)
-                        Door_timer = time.NewTimer(3*time.Second) //nsek timer
-                        }
+                        Door_timer = time.NewTimer(3*time.Second)
                     }
+                }
+
                 /*
                 fmt.Printf("%+v\n", a)
                 if a == numFloors-1 {
